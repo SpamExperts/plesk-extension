122856682e724e32048a2d5fe1f1902279c9a7e8
Dmitry Lomakin <dmitry@spamexperts.com>
2016-08-03 12:22:37 +0300

    The "protect" & "unprotect" action work only if the request type is POST
    Refs #27900

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -258,31 +258,33 @@ class IndexController extends pm_Controller_Action
     {
         $messages = [];
 
-        foreach ((array) $this->_getParam('ids') as $domain) {
-            try {
-                $pleskDomain = new Modules_SpamexpertsExtension_Plesk_Domain($domain);
+        if ($this->_request->isPost()) {
+            foreach ((array)$this->_getParam('ids') as $domain) {
+                try {
+                    $pleskDomain = new Modules_SpamexpertsExtension_Plesk_Domain($domain);
 
-                if (!pm_Session::getClient()->hasAccessToDomain($pleskDomain->getId())) {
-                    $this->_status->addMessage(
-                        'error',
-                        sprintf('Access denied to the domain %s.', htmlentities($domain, ENT_QUOTES, 'UTF-8'))
+                    if (!pm_Session::getClient()->hasAccessToDomain($pleskDomain->getId())) {
+                        $this->_status->addMessage(
+                            'error',
+                            sprintf('Access denied to the domain %s.', htmlentities($domain, ENT_QUOTES, 'UTF-8'))
+                        );
+                        $this->_forward('domains');
+                    }
+
+                    $spamfilterDomain = new Modules_SpamexpertsExtension_SpamFilter_Domain($pleskDomain);
+                    $spamfilterDomain->protect(
+                        0 < pm_Settings::get(Modules_SpamexpertsExtension_Form_Settings::OPTION_AUTO_PROVISION_DNS)
                     );
-                    $this->_forward('domains');
+                    $messages[] = [
+                        'status' => 'info',
+                        'content' => "Domain '{$domain}' has been successfully protected",
+                    ];
+                } catch (Exception $e) {
+                    $messages[] = [
+                        'status' => 'error',
+                        'content' => $e->getMessage(),
+                    ];
                 }
-
-                $spamfilterDomain = new Modules_SpamexpertsExtension_SpamFilter_Domain($pleskDomain);
-                $spamfilterDomain->protect(
-                    0 < pm_Settings::get(Modules_SpamexpertsExtension_Form_Settings::OPTION_AUTO_PROVISION_DNS)
-                );
-                $messages[] = [
-                    'status' => 'info',
-                    'content' => "Domain '{$domain}' has been successfully protected",
-                ];
-            } catch (Exception $e) {
-                $messages[] = [
-                    'status' => 'error',
-                    'content' => $e->getMessage(),
-                ];
             }
         }
 
@@ -293,31 +295,33 @@ class IndexController extends pm_Controller_Action
     {
         $messages = [];
 
-        foreach ((array) $this->_getParam('ids') as $domain) {
-            try {
-                $pleskDomain = new Modules_SpamexpertsExtension_Plesk_Domain($domain);
+        if ($this->_request->isPost()) {
+            foreach ((array)$this->_getParam('ids') as $domain) {
+                try {
+                    $pleskDomain = new Modules_SpamexpertsExtension_Plesk_Domain($domain);
 
-                if (!pm_Session::getClient()->hasAccessToDomain($pleskDomain->getId())) {
-                    $this->_status->addMessage(
-                        'error',
-                        sprintf('Access denied to the domain %s.', htmlentities($domain, ENT_QUOTES, 'UTF-8'))
+                    if (!pm_Session::getClient()->hasAccessToDomain($pleskDomain->getId())) {
+                        $this->_status->addMessage(
+                            'error',
+                            sprintf('Access denied to the domain %s.', htmlentities($domain, ENT_QUOTES, 'UTF-8'))
+                        );
+                        $this->_forward('domains');
+                    }
+
+                    $spamfilterDomain = new Modules_SpamexpertsExtension_SpamFilter_Domain($pleskDomain);
+                    $spamfilterDomain->unprotect(
+                        0 < pm_Settings::get(Modules_SpamexpertsExtension_Form_Settings::OPTION_AUTO_PROVISION_DNS)
                     );
-                    $this->_forward('domains');
+                    $messages[] = [
+                        'status' => 'info',
+                        'content' => "Domain '{$domain}' has been successfully unprotected",
+                    ];
+                } catch (Exception $e) {
+                    $messages[] = [
+                        'status' => 'error',
+                        'content' => $e->getMessage(),
+                    ];
                 }
-
-                $spamfilterDomain = new Modules_SpamexpertsExtension_SpamFilter_Domain($pleskDomain);
-                $spamfilterDomain->unprotect(
-                    0 < pm_Settings::get(Modules_SpamexpertsExtension_Form_Settings::OPTION_AUTO_PROVISION_DNS)
-                );
-                $messages[] = [
-                    'status' => 'info',
-                    'content' => "Domain '{$domain}' has been successfully unprotected",
-                ];
-            } catch (Exception $e) {
-                $messages[] = [
-                    'status' => 'error',
-                    'content' => $e->getMessage(),
-                ];
             }
         }
 

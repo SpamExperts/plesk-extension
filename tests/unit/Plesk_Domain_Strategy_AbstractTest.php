63fd18437cacb5c4617e1481ccfd4dfa6f1e8215
Dmitry Lomakin <dmitry@spamexperts.com>
2016-08-11 16:24:32 +0200

    Unit tests for Modules_SpamexpertsExtension_Plesk_Domain_Strategy_Abstract() ...
    .. are on the design stage yet :)
    
    Refs #27900

---
+++ tests/unit/Plesk_Domain_Strategy_AbstractTest.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once LIB_ROOT . '/Plesk/Domain/Strategy/Abstract.php';
+
+class Plesk_Domain_Strategy_AbstractTest extends \PHPUnit_Framework_TestCase
+{
+    // tests
+    public function testProtectAsDomainLocalDomainGetsProtectedInSkipRemoteDomainsMode()
+    {
+    }
+
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object    Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     */
+    protected function invokeMethod(&$object, $methodName, array $parameters = [])
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+
+}

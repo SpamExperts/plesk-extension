66f204fdd187316c34e2e04452b62ef31de109cf
Claudia Petre <claudia.petre2010@gmail.com>
2016-11-28 16:23:21 +0100

    Refs #28857 Add method to retrieve customer domains at reseller level

--- plib/library/Plesk/ApiClient.php
+++ plib/library/Plesk/ApiClient.php
@@ -12,4 +12,56 @@ class Modules_SpamexpertsExtension_Plesk_ApiClient
         return (string) $api->xmlapi('<server><get><gen_info/></get></server>')['version'];
     }
 
+    public function getDomains($resellerId)
+    {
+        $domains = [];
+
+        $request = <<<APICALL
+<customer>
+    <get>
+      <filter>
+          <owner-id>$resellerId</owner-id>
+      </filter>
+      <dataset>
+        <gen_info>
+        </gen_info>
+      </dataset>
+    </get>
+</customer>
+APICALL;
+
+        $response = $this->xmlapi($request);
+
+        $filter = [];
+
+        foreach ($response->customer->get->result as $customer) {
+            $filter[] = "<get-domain-list><filter><id>".$customer->id->__toString()."</id></filter></get-domain-list>";
+        }
+
+        if (!empty($filter)) {
+            $filter = implode("", $filter);
+
+            $request = <<<APICALL
+<customer>
+    $filter
+</customer>
+APICALL;
+
+            $response = $this->xmlapi($request);
+
+            foreach ($response->customer->children() as $customerDomains) {
+                foreach ($customerDomains->result->domains as $domain) {
+                    foreach ($domain->children() as $item) {
+                        $domains[] = [
+                            "id" => $item->id->__toString(),
+                            "name" => $item->name->__toString(),
+                            "type" => $item->type->__toString()
+                        ];
+                    }
+                }
+            }
+        }
+
+        return $domains;
+    }
 }
